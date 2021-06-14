6e18ae980151df8611928512b657cab8166aa687
oyk09 <oyk09@sjinc.co.kr>
2021-06-15 01:35:13 +0900

    test

---
+++ apiProject/src/main/java/com/example/spring/api/ApiProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.spring.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProjectApplication.class, args);
+	}
+
+	//https://hanhyx.tistory.com/39
+}
